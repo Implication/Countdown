e325fc895521bc577d9199d3915275c0d08b2eb8
Implication <IamTrajon@gmail.com>
2020-07-19 19:59:08 -0700

    Updates

---
+++ precache-manifest.39f1fba3bbd9ab5d571ccfde7b9582d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb52c6e7f63b96e815346b2a5b2de64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d3b1b5d219fe4baa9d9",
+    "url": "/static/css/main.2bcf6c7a.chunk.css"
+  },
+  {
+    "revision": "70a5b1b8207bec3fd1db",
+    "url": "/static/js/2.b7c14901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7c14901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3b1b5d219fe4baa9d9",
+    "url": "/static/js/main.d348697b.chunk.js"
+  },
+  {
+    "revision": "a8cd4cbc1e3c1b04920b",
+    "url": "/static/js/runtime-main.8e37bc8e.js"
+  }
+]);
